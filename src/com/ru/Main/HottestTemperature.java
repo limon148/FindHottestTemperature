76ddef2513668ba5db88d9328bf567e0e7bde88d
limon148 <mehadihasan.cseru@gmail.com>
2017-09-28 11:30:48 -0700

    this method testHottestTemperatureInDay can read new file

--- src/com/ru/Main/HottestTemperature.java
+++ src/com/ru/Main/HottestTemperature.java
@@ -6,7 +6,12 @@ import org.apache.commons.csv.*;
 public class HottestTemperature {
 
 	public static void main(String[] args) {
-		//HottestTemperature ob = new HottestTemperature();
-		//ob.testHottestTemperatureInDay();
+		HottestTemperature ob = new HottestTemperature();
+		ob.testHottestTemperatureInDay();
 	}
+	private void testHottestTemperatureInDay() {
+		FileResource fr = new FileResource();
+		
+	}
+
 }
