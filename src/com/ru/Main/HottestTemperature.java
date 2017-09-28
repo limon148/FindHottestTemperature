4ed51aae5eb11e93cd5a8af5dbddbfe4b0fdbef2
limon148 <mehadihasan.cseru@gmail.com>
2017-09-28 12:53:19 -0700

    this method hottestHourInFile will return the hottest temperature of the day and tester method will test this method

--- src/com/ru/Main/HottestTemperature.java
+++ src/com/ru/Main/HottestTemperature.java
@@ -11,7 +11,25 @@ public class HottestTemperature {
 	}
 	private void testHottestTemperatureInDay() {
 		FileResource fr = new FileResource();
-		
+		CSVRecord largest = hottestHourInFile(fr.getCSVParser());
+		System.out.println("Hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+	}
+	private CSVRecord hottestHourInFile(CSVParser csvParser) {
+		CSVRecord largestSoFar = null;
+		for(CSVRecord currentRow : csvParser){
+			if(largestSoFar == null){
+				largestSoFar = currentRow;
+			}
+			else{
+				double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+				double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+				if(currentTemp > largestTemp){
+					largestSoFar = currentRow;
+				}
+				
+			}
+		}
+		return largestSoFar;
 	}
 
 }
