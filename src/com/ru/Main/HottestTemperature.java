690a4c66f7d052109798f23cb98a33db2b4cee3b
limon148 <mehadihasan.cseru@gmail.com>
2017-09-30 00:51:04 -0700

    this method hottestTemperatureInManyDays will return the row that day the temperature was high on that file

--- src/com/ru/Main/HottestTemperature.java
+++ src/com/ru/Main/HottestTemperature.java
@@ -2,23 +2,38 @@ package com.ru.Main;
 
 import edu.duke.*;
 import org.apache.commons.csv.*;
+import java.io.*;
 
 public class HottestTemperature {
 
 	public static void main(String[] args) {
 		HottestTemperature ob = new HottestTemperature();
 		ob.testHottestTemperatureInDay();
-		HottestTemperature ob1 = new HottestTemperature();
-		ob1.testHottestTemperatureInManyDays();
+		ob.testHottestTemperatureInManyDays();
 	}
 	private void testHottestTemperatureInManyDays() {
 		CSVRecord largest = hottestTemperatureInManyDays();
-		//System.out.println("Hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("TimeEST"));
+		System.out.println("Hottest temperature was " + largest.get("TemperatureF") + " at " + largest.get("DateUTC"));
 		
 	}
 	private CSVRecord hottestTemperatureInManyDays() {
-		
-		return null;
+		CSVRecord largestSoFar = null;
+		DirectoryResource dr = new DirectoryResource();
+		for(File f : dr.selectedFiles()){
+			FileResource fr = new FileResource(f);
+			CSVRecord currentRow = hottestHourInFile(fr.getCSVParser());
+			if(largestSoFar == null){
+				largestSoFar = currentRow;
+			}
+			else{
+				double currentTemp = Double.parseDouble(currentRow.get("TemperatureF"));
+				double largestTemp = Double.parseDouble(largestSoFar.get("TemperatureF"));
+				if(currentTemp > largestTemp){
+					largestSoFar = currentRow;
+				}
+			}
+		}
+		return largestSoFar;
 	}
 	private void testHottestTemperatureInDay() {
 		FileResource fr = new FileResource();
